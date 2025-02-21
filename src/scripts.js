da963b158edd7bb2074cc716cfa5d2db6f2e25e0
Gabriel Pimentel <gabrielpimentel@live.com>
2025-02-20 22:41:27 -0300

    Add files via upload

--- src/scripts.js
+++ src/scripts.js
@@ -1,10 +1,4 @@
-// js/scripts.js
 function toggleMenu() {
     const menu = document.querySelector('.menu');
     menu.classList.toggle('active');
 }
-
-document.addEventListener("DOMContentLoaded", function() {
-    const menuIcon = document.querySelector('.menu-icon');
-    menuIcon.addEventListener("click", toggleMenu);
-});
